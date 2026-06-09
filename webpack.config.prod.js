1c4992508a82c40b208e0325734b906586adaaa9
mkostenko98 <m.kostenkomimimi@gmail.com>
2026-06-09 19:53:02 +0300

    добавила listen section

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -11,7 +11,7 @@ module.exports = merge(common, {
     }),
     new CopyPlugin({
       patterns: [
-        { from: 'img', to: 'img' },
+        // { from: 'img', to: 'img' },
         { from: 'css', to: 'css' },
         // { from: 'js/vendor', to: 'js/vendor' },
         // { from: 'icon.svg', to: 'icon.svg' },
